1bfe8abbe35130c5fd93c262f940b743b632c177
Daniel Warner <danielwarner00@gmail.com>
2025-01-19 18:10:43 -0800

    add usb data pins to top testbench

--- tb_top.v
+++ tb_top.v
@@ -2,8 +2,8 @@ module tb_top;
     
     reg clock = 0;
     always #10.4166667ns clock <= ~clock; // 48 mhz
-    wire r, g, b;
-    top top(clock, r, g, b);
+    wire usb_d_p, usb_d_n, usb_pullup, r, g, b;
+    top top(clock, usb_d_p, usb_d_n, usb_pullup, r, g, b);
 
     initial $monitor(r);
 endmodule
