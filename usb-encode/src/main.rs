17be9b38c181b55e03d34bc8827345e9d691dca5
Daniel Warner <danielwarner00@gmail.com>
2025-02-02 16:03:45 -0800

    add bit stuffing decoding to usb
    also add encoding for test code

--- usb-encode/src/main.rs
+++ usb-encode/src/main.rs
@@ -8,24 +8,81 @@ fn main() {
     let mut stdout = std::io::stdout();
     stdin.read_to_string(&mut buffer).unwrap();
 
-    // the last bit in the sync pattern is 0
-    let mut previous_output = false;
-
     let mut output = String::new();
-    for bit in buffer.split_whitespace().map(|substring| match substring {
+    for bit in UsbEncoder::from(buffer.split_whitespace().map(|substring| match substring {
         "0" => false,
         "1" => true,
         _ => panic!(),
-    }) {
-        // 1 is represented by no change in level, 0 is represented by a change in level
-        if !(bit ^ previous_output) {
-            previous_output = true;
+    })) {
+        if bit {
             output.push_str("1 ");
         } else {
-            previous_output = false;
             output.push_str("0 ");
         }
     }
 
     stdout.write(output.as_bytes()).unwrap();
+}
+
+struct UsbEncoder<I: Iterator<Item=bool>> {
+    inner: I,
+    consecutive_input_ones: i32,
+    previous_output: bool,
+}
+
+impl<I: Iterator<Item=bool>> From<I> for UsbEncoder<I> {
+    fn from(iterator: I) -> Self {
+        UsbEncoder {
+            inner: iterator,
+            consecutive_input_ones: 1, // since the sync packet ends with an encoded one
+            previous_output: false, // since the sync packet ends at low level
+        }
+    }
+
+}
+
+impl<I: Iterator<Item=bool>> Iterator for UsbEncoder<I> {
+    type Item = bool;
+
+    fn next(&mut self) -> Option<bool> {
+        let output = match self.consecutive_input_ones {
+            7.. => panic!(),
+            6 => {
+                self.consecutive_input_ones = 0;
+                Some(!self.previous_output)
+            }
+            _ => if let Some(inner_next) = self.inner.next() {
+                    self.consecutive_input_ones = if inner_next {
+                        self.consecutive_input_ones + 1 
+                    } else {
+                        0
+                    };
+                    Some(!(inner_next ^ self.previous_output))
+                } else {
+                    None
+                }
+        };
+
+        if let Some(output) = output {
+            self.previous_output = output;
+        }
+
+        output
+    }
+}
+
+#[test]
+fn encode_test() {
+    assert_eq!(encode_str("1111"), "0000");
+    assert_eq!(encode_str("11111"), "000001");
+    assert_eq!(encode_str("0011"), "1000");
+    assert_eq!(encode_str("111111"), "0000011");
+}
+
+fn encode_str(s: &str) -> String {
+    UsbEncoder::from(s.chars().map(|c| match c {
+        '0' => false,
+        '1' => true,
+        _ => panic!(),
+    })).map(|bit| if bit { '1' } else { '0' }).collect()
 }
--- usb.v
+++ usb.v
@@ -21,7 +21,7 @@ module usb(clock48, usb_d_p, usb_d_n, usb_pullup, packet_ready);
     output reg packet_ready = 0;
     reg next_packet_ready;
 
-    reg [$clog2(32) - 1:0] bits_to_read, next_bits_to_read;
+    reg [$clog2(32):0] bits_to_read, next_bits_to_read;
     reg [$clog2(1024/4) - 1:0] buffer_write_index = 0, next_buffer_write_index;
 
     reg [3:0] state = STATE_POWERED, next_state;
@@ -33,14 +33,20 @@ module usb(clock48, usb_d_p, usb_d_n, usb_pullup, packet_ready);
     wire data_j = differential_1;
     wire data_k = differential_0;
     wire idle = usb_d_p && !usb_d_n; // equivalent to differential_1 and data_j
+    // this is the undecoded bit sent over the wire
     wire data = data_j;
 
     reg [1:0] data_ready_counter = 0, next_data_ready_counter;
-    wire data_ready = data_ready_counter == 3;
-    // bits are sent least-significant bit first
-    wire [31:0] read_bits = { !(data ^ previous_data), previous_read_bits[31:1] };
+    wire data_ready = data_ready_counter == 3 && !skip_bit;
     reg previous_data, next_previous_data;
-    reg [31:0] previous_read_bits, next_previous_read_bits;
+
+    wire read_complete = bits_to_read == 0;
+    reg [2:0] consecutive_nzri_data_ones = 0;
+    wire skip_bit = consecutive_nzri_data_ones >= 6;
+    wire nzri_decoded_data = !(data ^ previous_data); // nzri decoded, but not bit-stuffing decoded
+    wire decoded_data = nzri_decoded_data; // only defined when !skip_bit
+    // bits are sent least-significant bit first
+    reg [31:0] read_bits; // these are the last 32 decoded bits
 
     reg write_to_packet_buffer;
 
@@ -53,13 +59,16 @@ module usb(clock48, usb_d_p, usb_d_n, usb_pullup, packet_ready);
         next_buffer_write_index = 8'bx;
         next_packet_ready = packet_ready;
 
-        if (data_ready) begin
+        if (data_ready_counter == 3) begin
             next_previous_data = data;
-            next_previous_read_bits = read_bits;
-            next_bits_to_read = bits_to_read - 1;
         end else begin
             next_previous_data = previous_data;
-            next_previous_read_bits = previous_read_bits;
+        end
+
+        if (data_ready) begin
+            next_previous_data = data;
+            next_bits_to_read = bits_to_read - 1;
+        end else begin
             next_bits_to_read = bits_to_read;
         end
 
@@ -74,43 +83,39 @@ module usb(clock48, usb_d_p, usb_d_n, usb_pullup, packet_ready);
                 if (data_k) begin
                     next_state = STATE_SYNCING;
                     next_previous_data = 1;
-                    next_bits_to_read = 7;
+                    next_bits_to_read = 8;
                     next_data_ready_counter = 2;
                 end
             end
             STATE_SYNCING: begin
                 next_data_ready_counter = data_ready_counter + 1;
-                if (data_ready) begin
-                    if (bits_to_read == 0) begin
-                        if (read_bits[31:24] == 8'b10000000) begin
-                            if (!top.core.control_status_registers.mip_meip) begin
-                                next_state = STATE_READING;
-                                next_bits_to_read = 31;
-                                next_buffer_write_index = 0;
-                            end else begin
-                                next_state = STATE_IGNORE_PACKET;
-                            end
+                if (read_complete) begin
+                    if (read_bits[31:24] == 8'b10000000) begin
+                        if (!top.core.control_status_registers.mip_meip) begin
+                            next_state = STATE_READING;
+                            next_bits_to_read = 32;
+                            next_buffer_write_index = 0;
                         end else begin
-                            `ifdef simulation
-                                $stop;
-                            `endif
+                            next_state = STATE_IGNORE_PACKET;
                         end
+                    end else begin
+                        `ifdef simulation
+                            $stop;
+                        `endif
                     end
                 end
             end
             STATE_READING: begin
                 next_data_ready_counter = data_ready_counter + 1;
-                if (data_ready) begin
-                    if (bits_to_read == 0) begin
-                        write_to_packet_buffer = 1;
+                if (read_complete) begin
+                    write_to_packet_buffer = 1;
 
-                        if (buffer_write_index == 8'h0) begin
-                            next_packet_ready = 1;
-                            next_state = STATE_READ_COMPLETE;
-                        end
-                        next_buffer_write_index = buffer_write_index + 1;
-                        next_bits_to_read = 31;
+                    if (buffer_write_index == 8'h0) begin
+                        next_packet_ready = 1;
+                        next_state = STATE_READ_COMPLETE;
                     end
+                    next_buffer_write_index = buffer_write_index + 1;
+                    next_bits_to_read = 32;
                 end
             end
             STATE_READ_COMPLETE: begin
@@ -127,6 +132,18 @@ module usb(clock48, usb_d_p, usb_d_n, usb_pullup, packet_ready);
             reset_counter <= 0;
         end
 
+        if (data_ready_counter == 3) begin
+            if (!skip_bit) begin
+                read_bits <= { decoded_data, read_bits[31: 1] };
+            end
+
+            if (nzri_decoded_data == 1) begin
+                consecutive_nzri_data_ones <= consecutive_nzri_data_ones + 1;
+            end else begin
+                consecutive_nzri_data_ones <= 0;
+            end
+        end
+
         state <= next_state;
         data_ready_counter <= next_data_ready_counter;
         buffer_write_index <= next_buffer_write_index;
@@ -135,11 +152,9 @@ module usb(clock48, usb_d_p, usb_d_n, usb_pullup, packet_ready);
         buffer_write_index <= next_buffer_write_index;
         packet_ready <= next_packet_ready;
         previous_data <= next_previous_data;
-        previous_read_bits <= next_previous_read_bits;
 
         if (write_to_packet_buffer) begin
             top.usb_packet_buffer[buffer_write_index] <= read_bits;
         end
     end
-
 endmodule
