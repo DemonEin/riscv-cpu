8a104d4144a0266b69ab73ce2cfed8dd0a5e20fb
Daniel Warner <danielwarner00@gmail.com>
2025-04-19 17:03:09 -0700

    use stdint.h instead of manual typedefs

--- lib/cpulib.h
+++ lib/cpulib.h
@@ -1,13 +1,4 @@
-// using ILP32 convention
-typedef signed char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef long long int64_t;
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+#include <stdint.h>
 
 #define CLOCK_FREQUENCY 12000000
 
